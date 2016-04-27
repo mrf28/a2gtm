21f89e9c21a470213a09c7e2d1190a9007654512
Marcos Regis Freitas <Marcos.freitas@its.local>
2016-04-27 13:23:44 -0300

    add list

--- app/board/board.component.ts
+++ app/board/board.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnInit, AfterViewInit} from 'angular2/core';
+import {Component, Input, OnInit, AfterViewInit, ElementRef} from 'angular2/core';
+import {Board} from '../board/board';
 import {Column} from '../column/column';
 import {BoardService} from './board.service';
 import {ColumnComponent} from '../column/column.component';
@@ -14,16 +15,19 @@ declare var jQuery: any;
 })
 export class BoardComponent implements OnInit {
     title: string = "This is a board";
-    columns: Column[];
-    constructor(private _boardService: BoardService){}
+    board: Board;
+    addingColumn = false;
+    addColumnText: string;
+    constructor(public el: ElementRef, private _boardService: BoardService){}
     
     ngOnInit(){
-        this.columns = this._boardService.getColumns('randomid');
+        this.board = this._boardService.getBoard('randomid');
         document.title = this.title + " | Generic Task Manager";
     }
 
     ngAfterViewInit(){
       jQuery('#main').sortable({
+        items: '.sortable-column',
         handler: '.header',
         connectWith: "#main",
         placeholder: "column-placeholder",
@@ -35,4 +39,37 @@ export class BoardComponent implements OnInit {
       });
       jQuery('#main').disableSelection();     
     }    
+
+    blurOnEnter(event) {
+        if (event.keyCode === 13) {
+            event.target.blur();
+        }
+    } 
+
+    enableAddColumn() {
+        this.addingColumn = true;
+        console.log(this.el);
+        let input = jQuery('.add-column')[0]
+            .getElementsByTagName('input')[0];
+
+        setTimeout(function() { input.focus(); }, 0);
+    }
+
+    addColumn() {
+        if (this.addColumnText && this.addColumnText.trim() !== '') {
+            let newColumn = <Column>{ title: this.addColumnText,
+                 order: this.board.columns.length + 1, 
+                 boardId: this.board.id,
+                 cards: []
+             };
+            this.board.columns.push(newColumn);
+            this._boardService.addColumn(newColumn);
+        }
+        this.clearAddColumn();
+    }
+
+    clearAddColumn() {
+        this.addingColumn = false;
+        this.addColumnText = '';
+    }
  }
