ff040e3975b0e8aed4c6d690378f38187d0c794a
Marcos Regis Freitas <Marcos.freitas@its.local>
2016-04-27 17:58:06 -0300

    service refactor and cards ordering save

--- app/board/board.service.ts
+++ app/board/board.service.ts
@@ -36,156 +36,145 @@ var DEFAULT_BOARD: Board = {
 };
 
 @Injectable()
-export class BoardService {
+export class BoardService implements OnInit {
 	private lastColumnId: number;
 	private lastCardId: number;
+	private Boards: Board[];
 
 	constructor(){
 		this.lastColumnId = 3;
 		this.lastCardId = 1;
 
-		if (!localStorage.hasOwnProperty("boards")) {
-			localStorage.setItem('boards', JSON.stringify([DEFAULT_BOARD]));
-		} else {
-      this.lastColumnId = this.countColumns();
-      this.lastCardId = this.countCards();
+		if (localStorage.hasOwnProperty("boards")) {
+			this.Boards = this.getBoardsLocal();
 		}
+		else {
+			this.Boards = [DEFAULT_BOARD];
+			localStorage.setItem('boards', JSON.stringify(this.Boards));
+		}
+
+		this.lastColumnId = this.countColumns();
+		this.lastCardId = this.countCards();
+	}
+
+	ngOnInit() {
+		console.log('service on init');
 	}
 
 	getBoards(boardId: string) {
-		if (localStorage.hasOwnProperty("boards")) {
-			let boards = this.getBoardsLocal();
-			
-			return boards;
-		}
-		else {
-			localStorage.setItem('boards', JSON.stringify([DEFAULT_BOARD]));
-		}
-
-		return [DEFAULT_BOARD];
+		
+		return this.Boards;
 	}
 
 	getBoard(boardId: string) {
-		if (localStorage.hasOwnProperty("boards")) {
-			let boards = this.getBoardsLocal();
-			if (boards.length > 0) {
-				let board = boards.filter(board => board.id === boardId)[0];
-				return board;
-			} else {
-				localStorage.setItem('boards', JSON.stringify([DEFAULT_BOARD]));
-				return DEFAULT_BOARD;
-			}
-		}
-
-		return DEFAULT_BOARD;
+		let board = this.Boards.filter(board => board.id === boardId)[0];
+		return board;
 	}
 
 	getColumns(boardId: string){
-		if (localStorage.hasOwnProperty("boards")) {
-			let boards = this.getBoardsLocal(); 
-			if (boards.length > 0){
-				let board = boards.filter(board => board.id === boardId)[0];
-
-				return board.columns;
-			} else {
-				return DEFAULT_COLUMNS;
-			}
-		}
-
-		return DEFAULT_COLUMNS;
+		let board = this.Boards.filter(board => board.id === boardId)[0];
+		return board.columns;
 	}
 
 	addColumn(column: Column){
-		if (localStorage.hasOwnProperty("boards")) {
-			let boards = this.getBoardsLocal();
-			if (boards.length > 0) {
-				let board = boards.filter(board => board.id === column.boardId)[0];
-				column.id = ++this.lastColumnId;
+		let board = this.Boards.filter(board => board.id === column.boardId)[0];
+		column.id = ++this.lastColumnId;
 
-				board.columns.push(column);
+		board.columns.push(column);
 
-				localStorage.setItem("boards", JSON.stringify(boards));
+		localStorage.setItem("boards", JSON.stringify(this.Boards));
 
-				return true;
-			} else {
-				return false;
-			}
-		}
-		return false;
+		return true;
 	}
 
 	updateColumn(column: Column) {
-		if(localStorage.hasOwnProperty("boards")) {
-			let boards = this.getBoardsLocal();
-			if (boards.length > 0) {
-				//let board = 
-				let board = boards.filter(board => board.id === column.boardId)[0];
+		let board = this.Boards.filter(board => board.id === column.boardId)[0];
 
-				let c = board.columns.filter(c => c.id === column.id)[0]
+		let c = board.columns.filter(c => c.id === column.id)[0]
 
-				c.title = column.title;
+		c.title = column.title;
 
-				console.log(JSON.stringify(boards));
+		console.log(JSON.stringify(this.Boards));
 
-				localStorage.setItem("boards", JSON.stringify(boards));
+		localStorage.setItem("boards", JSON.stringify(this.Boards));
 
-				return true;
-			} else {
-				return false;
-			}
-		}
-		return false;
+		return true;
 	}
 
-
 	addCard(card: Card, column: Column) {
-		if (localStorage.hasOwnProperty("boards")) {
-			let boards = this.getBoardsLocal();
-			if (boards.length > 0) {
-				let board = boards.filter(board => board.id === column.boardId)[0];
+		let board = this.Boards.filter(board => board.id === column.boardId)[0];
 				
-				let c = board.columns.filter(c => c.id === column.id)[0];
-				
-				card.id = ++this.lastCardId;
+		let c = board.columns.filter(c => c.id === column.id)[0];
+		
+		card.id = ++this.lastCardId;
 
-				c.cards.push(card);
+		c.cards.push(card);
 
-				localStorage.setItem("boards", JSON.stringify(boards));
+		localStorage.setItem("boards", JSON.stringify(this.Boards));
 
-				return true;
-			} else {
-				return false;
-			}
-		}
-		return false;
+		return true;
 	}
 
 	updateCard(card: Card) {
-		if (localStorage.hasOwnProperty("boards")) {
-			let boards = this.getBoardsLocal();
-			if (boards.length > 0) {
-				let col = this.findColumnById(card.columnId, boards);
+		let col = this.findColumnById(card.columnId, this.Boards);
 
-				let board = boards.filter(board => board.id === col.boardId)[0];
+		let board = this.Boards.filter(board => board.id === col.boardId)[0];
 
-				let column = board.columns.filter(x => x.id === card.columnId)[0];
-				
-				let c = column.cards.filter(x => x.id === card.id)[0];
+		let column = board.columns.filter(x => x.id === card.columnId)[0];
+		
+		let c = column.cards.filter(x => x.id === card.id)[0];
 
-				c.title = card.title;
-				c.order = card.order;
-				c.columnId = card.columnId;
+		c.title = card.title;
+		c.order = card.order;
+		c.columnId = card.columnId;
 
-				localStorage.setItem("boards", JSON.stringify(boards));
+		localStorage.setItem("boards", JSON.stringify(this.Boards));
 
-				return true;
-			} else {
-				return false;
-			}
-		}
-		return false;
+		return true;
 	}
 
+	reorderCard(cardId, targetColumnId, senderColumnId, index, boardId) {
+		let board = this.Boards.filter(x => x.id === boardId)[0];
+		let senderColumn: Column;
+		let targetColumn: Column;
+
+		for (let c in board.columns){
+			if (board.columns[c].id == senderColumnId){
+				senderColumn = board.columns[c];
+			} else if (board.columns[c].id == targetColumnId) {
+				targetColumn = board.columns[c];
+			}
+		}
+		
+		let i = 0;
+		if (targetColumnId !== senderColumnId){
+			let currentCard: Card;
+			for (let c in senderColumn.cards) {
+				let card = senderColumn.cards[c];
+				if (card.id === cardId){
+					card.order = i;
+					card.columnId = senderColumnId;
+					currentCard = card;
+					console.log(currentCard);
+				} else {
+					card.order = i++;
+				}
+			}
+
+			console.log(currentCard, index);
+
+			senderColumn.cards.splice(currentCard.order, 1);
+
+			targetColumn.cards.splice(index, 0, currentCard);
+		}
+		i = 0;
+		for (let c in targetColumn.cards) {
+			let card = targetColumn.cards[c];
+			card.order = i++;
+		}
+
+		localStorage.setItem("boards", JSON.stringify(this.Boards));
+	}
 
 	private findColumnById(id: number, boards: Board[]): Column {
 		for (let b in boards){
@@ -197,18 +186,18 @@ export class BoardService {
 	}
 
 	private getBoardsLocal(): Board[]{
-		return <Board[]>JSON.parse(localStorage.getItem("boards"));
+		return <Board[]>JSON.parse(localStorage['boards']);
 	}
 
 	private countCards(): number{
-    let i = 0;
-    this.getBoardsLocal().filter(x => !!x.columns.filter(y => !!y.cards.filter(z => !!i++)));
-    return i;
+	    let i = 0;
+	    this.Boards.filter(x => !!x.columns.filter(y => !!y.cards.filter(z => !!i++)));
+	    return i;
 	}
 
 	private countColumns(): number {
-    let i = 0;
-    this.getBoardsLocal().filter(x => !!x.columns.filter(y => !!i++));
-    return i;
-  }
+	    let i = 0;
+		this.Boards.filter(x => !!x.columns.filter(y => !!i++));
+	    return i;
+  	}
 }
--- app/column/column.component.ts
+++ app/column/column.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, AfterViewInit, ElementRef} from 'angular2/core';
+import {Component, Input, OnInit, AfterViewInit, ElementRef, EventEmitter} from 'angular2/core';
 import {Column} from './column';
 import {Card} from '../card/card';
 import {CardComponent} from '../card/card.component'
@@ -20,7 +20,12 @@ export class ColumnComponent implements OnInit {
     addCardText: string;
     currentTitle: string;
 
-    constructor(private el: ElementRef, private _boardService: BoardService) { }
+    public cardReorder: EventEmitter<any>;
+
+    constructor(private el: ElementRef, private _boardService: BoardService) { 
+        this.cardReorder = new EventEmitter();
+        this.cardReorder.subscribe(event => this.updateCardsOrder(event));
+    }
 
     ngOnInit(){
    
@@ -28,6 +33,7 @@ export class ColumnComponent implements OnInit {
 
     ngAfterViewInit(){
       let component = this;
+      var startColumn;
       jQuery('.card-list').sortable({
         connectWith: ".card-list",
         placeholder: "card-placeholder",
@@ -35,19 +41,29 @@ export class ColumnComponent implements OnInit {
         tolerance: 'pointer',
         start: function(event, ui) {
           ui.placeholder.height(ui.item.outerHeight());
+          startColumn = ui.item.parent();
         },
         stop: function(event, ui) {
-          component.updateCardOrder(event, ui);
+            var senderColumnId = +startColumn.attr('column-id');
+            var targetColumnId = +ui.item.closest('.card-list').attr('column-id');
+            var cardId = +ui.item.find('.card').attr('card-id');
+            var index = component.findCardIndex(cardId, ui.item.closest('.card-list'));
+
+            component.cardReorder.emit({
+                senderColumnId: senderColumnId,
+                targetColumnId: targetColumnId,
+                cardId: cardId,
+                index: index,
+                boardId: component.column.boardId
+            });
         }
       });
       jQuery('.card-list').disableSelection();     
     }
 
-    updateCardOrder(event, ui){
-        var senderColumnId = ui//.sender//.context.getAttribute('column-id');
-        var targetColumnId = +ui.item.closest('.card-list').attr('column-id');
-        var cardId = +ui.item.find('.card').attr('card-id');
-        console.log(senderColumnId, targetColumnId, cardId);
+    updateCardsOrder(event){
+        console.log(event);
+        this._boardService.reorderCard(event.cardId, event.targetColumnId, event.senderColumnId, event.index, event.boardId);
     }
 
     blurOnEnter(event) {
@@ -98,5 +114,16 @@ export class ColumnComponent implements OnInit {
     clearAddCard(){
         this.addingCard = false;
         this.addCardText = '';
+    }
+
+    private findCardIndex(cardId, columnEl){
+        let i = 0;
+        for (i = 0; i < columnEl.find('.card').length - 1; i++) {
+            if (columnEl.find('.card')[i].getAttribute('card-id') == cardId){
+                return i;
+            }
+         } 
+
+        return i;
     }
 }
