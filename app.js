e975268dc40e9b0ea5057438852e44f831673b98
Marcos <mrf_28@hotmail.com>
2016-04-27 23:25:11 -0300

    simple web server

---
+++ app.js
@@ -0,0 +1,47 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+
+function send404(response) {
+  response.writeHead(404, {"Content-type" : "text/plain"});
+  response.write("Error 404: resource not found");
+  response.end();
+}
+
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "/index.html";
+  } else {
+    filePath = request.url;
+  }
+
+  var absPath = __dirname  + filePath;
+  //console.log(absPath);
+  serverWorking(response, absPath);
+});
+
+var port_number = server.listen(process.env.PORT || 3000);
