c9884761dfdaeb094d565e816c11dedaacad3b74
Marcos Regis Freitas <Marcos.freitas@its.local>
2016-04-29 15:05:16 -0300

    minor fixes

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
+import {enableProdMode}    from 'angular2/core'; 
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
-
+enableProdMode();
 bootstrap(AppComponent);
